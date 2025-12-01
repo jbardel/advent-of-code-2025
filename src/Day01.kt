e50b94cf74fd2a9358c4ab2a9ecb584aa3a46030
Julien Bardel <julien.bardel@harvest.fr>
2025-12-01 13:31:01 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,49 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var curr = 50
+        var pass = 0
+
+        for (l in input) {
+            val direction = l.first()
+            val ticks = l.substring(1).toInt()
+
+            val step = if (direction == 'L') -1 else 1
+
+            for (i in 1..ticks) {
+                curr = (curr + step + 100) % 100
+            }
+
+            if (curr == 0) pass++
+        }
+        return pass
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var curr = 50
+        var pass = 0
+
+        for (l in input) {
+            val direction = l.first()
+            val ticks = l.substring(1).toInt()
+
+            val step = if (direction == 'L') -1 else 1
+
+            for (i in 1..ticks) {
+                curr = (curr + step + 100) % 100
+                if (curr == 0) pass++
+            }
+
+        }
+        return pass
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    
+    val testInput = readInput("test_input")
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
+    
+    val testInput2 = readInput("input")
+    part1(testInput2).println()
+    part2(testInput2).println()
 }
